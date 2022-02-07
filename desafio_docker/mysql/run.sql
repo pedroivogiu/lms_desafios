150d8f95a7469a3f36aa7b2c51303ed62a8d17c3
Pedro Ivo <pedrin.ivo@gmail.com>
2022-02-07 11:58:17 -0300

    Fechando desafio

--- desafio_docker/mysql/run.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE people(id int not null auto_increment PRIMARY KEY, name varchar(40) not null);
---
+++ desafio_docker/scripts/run.sql
@@ -0,0 +1,1 @@
+CREATE TABLE IF NOT EXISTS nodedb.people(id int not null auto_increment PRIMARY KEY, name varchar(40) not null);
